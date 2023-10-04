ce1a4b84a897a3faf4d6bad3ce48678b351a0fa8
HJ5990 <memory_144@naver.com>
2023-10-04 17:49:33 +0900

    231004

--- todolist/index2.js
+++ todolist/index2.js
@@ -2,7 +2,7 @@ function addTodo(){
     let inputText = document.getElementById('search-input').value;
     
     // 만약 내용이 없다면 함수 종료
-    if (searchInput.value.replace(/ /g,"") === '')
+    if (inputText.replace(/ /g,"") === '')
     return;
 
 }
